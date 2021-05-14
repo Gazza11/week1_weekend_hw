dbba281f2460e124f1a42545efacf26e2322b209
Gary Darnell <gdarnell9@gmail.com>
2021-05-14 13:50:20 +0100

    Tenth test passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -25,6 +25,9 @@ def get_pets_by_breed(pet_shop, breed_input):
         if pet['breed'] == breed_input:
             amount.append(breed_input)
         
-    
     return amount
 
+def find_pet_by_name(pet_shop, name):
+    for pet in pet_shop['pets']:
+        if pet['name'] == name:
+            return pet
