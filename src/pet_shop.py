36e4669c286d3b0784ba6989797d917eb4ff94c6
Gary Darnell <gdarnell9@gmail.com>
2021-05-14 12:57:56 +0100

    Seventh test passed

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -15,3 +15,6 @@ def get_pets_sold(pet_shop):
 def increase_pets_sold(pet_shop, pets_added):
     pet_shop['admin']['pets_sold'] = pet_shop['admin']['pets_sold'] + pets_added
     return pet_shop['admin']['pets_sold']
+
+def get_stock_count(pet_shop):
+    return len(pet_shop['pets'])
